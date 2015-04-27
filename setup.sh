7cf2ca11653d364e52b0c113c970791df1617f21
Jason Chaffee <jchaffee@sunverge.com>
2015-04-27 11:57:08 -0700

    changed color to auto in case terminal does not support color

--- setup.sh
+++ setup.sh
@@ -2,17 +2,17 @@
 
 git config --global core.editor vim
 git config --global core.excludesfile ~/.gitignore
-git config --global color.ui true
-git config --global color.interactive true
-git config --global color.status true
-git config --global color.branch true
-git config --global color.diff true
+git config --global color.ui auto
+git config --global color.interactive auto
+git config --global color.status auto
+git config --global color.branch auto
+git config --global color.diff auto
 git config --global push.default simple
 
-if [ "$GIT_USER_NAME" ]; then
+if [[ "$GIT_USER_NAME" ]]; then
     git config --global user.name "$GIT_USER_NAME"
 fi
 
-if [ "$GIT_USER_EMAIL" ]; then
+if [[ "$GIT_USER_EMAIL" ]]; then
     git config --global user.email "$GIT_USER_EMAIL"
 fi
