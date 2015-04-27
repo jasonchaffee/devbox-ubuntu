5f7460df62ae6461d07b2d4c58bbdc4d8dc0a2ad
Jason Chaffee <jchaffee@sunverge.com>
2015-04-27 12:04:24 -0700

    changed to use auto for git color

--- setup.sh
+++ setup.sh
@@ -2,11 +2,13 @@
 
 git config --global core.editor vim
 git config --global core.excludesfile ~/.gitignore
-git config --global color.ui auto
-git config --global color.interactive auto
-git config --global color.status auto
+
 git config --global color.branch auto
 git config --global color.diff auto
+git config --global color.interactive auto
+git config --global color.status auto
+git config --global color.ui auto
+
 git config --global push.default simple
 
 if [[ "$GIT_USER_NAME" ]]; then
